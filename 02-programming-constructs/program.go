67b3552f125706cccfd27c44a882b0821211644f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-11 14:57:50 +0530

    switch case & fallthrough

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -46,4 +46,101 @@ func main() {
 	fmt.Printf("x = %d\n", x)
 
 	//switch
+	/*
+		rank by score
+		score 0-3 => "Terrible"
+		score 4-7 => "Not Bad"
+		score 8-9 => "Very Good"
+		score 10 => "Excellent"
+		otherwise => "Invalid score"
+	*/
+	score := 6
+	/* switch score {
+	case 0:
+		fmt.Println("Terrible")
+	case 1:
+		fmt.Println("Terrible")
+	case 2:
+		fmt.Println("Terrible")
+	case 3:
+		fmt.Println("Terrible")
+	case 4:
+		fmt.Println("Not Bad")
+	case 5:
+		fmt.Println("Not Bad")
+	case 6:
+		fmt.Println("Not Bad")
+	case 7:
+		fmt.Println("Not Bad")
+	case 8:
+		fmt.Println("Very good")
+	case 9:
+		fmt.Println("Very good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid score")
+	} */
+
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6, 7:
+		fmt.Println("Not Bad")
+	case 8, 9:
+		fmt.Println("Very Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid score")
+	}
+
+	no := 15
+	switch {
+	case no%2 == 0:
+		fmt.Printf("%d is even\n", no)
+	case no%2 == 1:
+		fmt.Printf("%d is odd\n", no)
+	}
+
+	//using fallthrough
+	n := 4
+	switch n {
+	case 0:
+		fmt.Println("is zero")
+	case 1:
+		fmt.Println("n <= 1")
+		fallthrough // the next case block is executed without checking the condition
+	case 2:
+		fmt.Println("n <= 2")
+		fallthrough
+	case 3:
+		fmt.Println("n <= 3")
+		fallthrough
+	case 4:
+		fmt.Println("n <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("n <= 5")
+		fallthrough
+	case 6:
+		fmt.Println("n <= 6")
+	case 7:
+		fmt.Println("n <= 7")
+		fallthrough
+	case 8:
+		fmt.Println("n <= 8")
+	}
+
+	subscription := "premium"
+	switch subscription {
+	case "premium":
+		fmt.Println("All premium features included")
+		fallthrough
+	case "pro":
+		fmt.Println("All pro features included")
+		fallthrough
+	case "free":
+		fmt.Println("All free features included")
+	}
 }
