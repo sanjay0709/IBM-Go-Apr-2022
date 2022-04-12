1f5a9c76515c201fb9763a0f9bc3473dbe9f196a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-12 10:29:45 +0530

    Higher order functions - closures

---
+++ 03-functions/demo-07.go
@@ -0,0 +1,28 @@
+/* Higher order functions - closures */
+package main
+
+import "fmt"
+
+func main() {
+	increment := getIncrement()
+	fmt.Println(increment()) // => 1
+	fmt.Println(increment()) // => 2
+	fmt.Println(increment()) // => 3
+	fmt.Println(increment()) // => 4
+}
+
+func getIncrement() func() int {
+	var counter int
+	increment := func() int {
+		counter++
+		return counter
+	}
+	return increment
+}
+
+
+func fn(){
+	var no int
+}
+
+fn()
