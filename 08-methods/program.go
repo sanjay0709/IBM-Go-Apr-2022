6dd5b26e7ea65f7e300f268db8e8db405becbbd2
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-13 12:54:27 +0530

    all about methods

--- 08-methods/program.go
+++ 08-methods/program.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type MyStr string //alias for string
+
+func (str MyStr) Length() int {
+	return len(str)
+}
 
 type Employee struct {
 	Id        int
@@ -22,4 +30,13 @@ func main() {
 	}
 	//WhoAmI(emp)
 	emp.WhoAmI()
+
+	s := MyStr("This is a dummy string") // converting the "string" data to "MyStr" type
+	fmt.Println(s.Length())
+
+	//type convertion
+	var f float32
+	var i int8 = 100
+	f = float32(i)
+	fmt.Println(f)
 }
