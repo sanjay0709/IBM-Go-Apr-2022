4feda35f2ccdfd86765b8bff67f0657573945b81
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-14 12:58:05 +0530

    assignment-11 - solution

---
+++ 11-concurrency/assignment-11.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := genPrimes()
+	for i := 0; i < 10; i++ {
+		primeNo := <-ch
+		fmt.Println(primeNo)
+	}
+}
+
+func genPrimes() <-chan int {
+	ch := make(chan int)
+	go func() {
+		counter := 10
+		no := 3
+		for {
+			if isPrime(no) {
+				ch <- no
+				time.Sleep(500 * time.Millisecond)
+				counter++
+			}
+			if counter <= 0 {
+				break
+			}
+			no++
+		}
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
