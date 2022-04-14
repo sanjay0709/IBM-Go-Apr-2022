9649794c521e13b87f24440d28746656f17bedb0
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-14 12:33:00 +0530

    receive-only and send-only channels

--- 11-concurrency/demo-11-c.go
+++ 11-concurrency/demo-11-c.go
@@ -5,16 +5,19 @@ import (
 )
 
 func main() {
-	ch := make(chan int) //share memory by communicating
+
 	fmt.Println("main started")
-	go add(100, 200, ch)
+	ch := add(100, 200)
 	result := <-ch
 	fmt.Println(result)
 	fmt.Println("main completed")
 }
 
-func add(x, y int, ch chan<- int) {
-	result := x + y
-	<-ch
-	ch <- result
+func add(x, y int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		result := x + y
+		ch <- result
+	}()
+	return ch
 }
