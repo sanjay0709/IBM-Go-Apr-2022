b4898c619c2144030aec097eb7964fd69b65e933
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-14 10:47:15 +0530

    waitgroup updates

--- 11-concurrency/demo-06.go
+++ 11-concurrency/demo-06.go
@@ -6,18 +6,17 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup
-
 func main() {
+	var wg sync.WaitGroup
 	fmt.Println("main started")
-	wg.Add(1) //initializing the counter to 1
-	go f1()   //scheduling this function for execution
+	wg.Add(1)  //initializing the counter to 1
+	go f1(&wg) //scheduling this function for execution
 	f2()
 	wg.Wait() //blocked until the wg counter becomes 0
 	fmt.Println("main completed")
 }
 
-func f1() {
+func f1(wg *sync.WaitGroup) {
 	fmt.Println("f1 started")
 	time.Sleep(5 * time.Second)
 	fmt.Println("f1 completed")
